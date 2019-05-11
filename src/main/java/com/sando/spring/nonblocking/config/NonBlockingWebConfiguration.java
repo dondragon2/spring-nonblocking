ac32753f287a5d0cc04bb75ae628f37b571a1b37
Donald Walters <donald.walters@gmail.com>
2019-05-11 11:52:04 -0500

    initial commit

---
+++ src/main/java/com/sando/spring/nonblocking/config/NonBlockingWebConfiguration.java
@@ -0,0 +1,15 @@
+package com.sando.spring.nonblocking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class NonBlockingWebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+        returnValueHandlers.add(new ObservableReturnValueHandler());
+    }
+}
